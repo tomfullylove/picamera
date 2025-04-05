90c642955222c43be440d8613b423af76ad7c06d
Tom Fullylove <t.fullylove@hotmail.co.uk>
2025-04-05 12:59:10 +0100

    Add logging

---
+++ utils/logger.py
@@ -0,0 +1,13 @@
+import logging
+
+from logging.handlers import RotatingFileHandler
+
+handler = RotatingFileHandler("app.log", maxBytes=1000000, backupCount=3)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[handler, logging.StreamHandler()]
+)
+
+logger = logging.getLogger("Logger")
